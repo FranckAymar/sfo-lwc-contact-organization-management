c0ee0a3dbb744f4d948d4de80da637d441e6eb10
Franck-Aymar <franck.braffouo@gmail.com>
2026-05-21 23:37:11 -0400

    logic account child 1 and 2

--- force-app/main/default/lwc/accountChild2/accountChild2.js
+++ force-app/main/default/lwc/accountChild2/accountChild2.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountClass.getAccounts'; 
 
-export default class AccountChild2 extends LightningElement {}
+export default class AccountChild2 extends LightningElement {
+
+    @api searchTextChild2;
+
+    columns = [
+        {
+            label: 'Id',
+            fieldName: 'Id'
+        },
+        {
+            label: 'Name',
+            fieldName: 'Name'
+        },
+        {
+            label: 'Actions',
+            fieldName: 'Actions',
+            type: 'button',
+            typeAttributes: {
+                label: 'View contacts',
+                value: 'view_contacts'
+            }
+        }
+    ]
+
+    currentId;
+    currentName;
+
+
+    handleRowAction(event){
+
+        if(event.detail.action.value === 'view_contacts'){
+
+            this.currentId = event.detail.row.Id;
+            this.currentName = event.detail.row.Name;
+        }
+    }
+
+    @wire(getAccounts, { searchTextClass: '$searchTextChild2'}) accountRecords; 
+}
